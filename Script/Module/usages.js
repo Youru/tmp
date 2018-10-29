7d4aac3dc002750aa082fd60c7567b945230ca7a
Uru <johann.vanbeveren@gmail.com>
2018-10-29 22:45:42 +0100

    mock front

--- Script/Module/usages.js
+++ Script/Module/usages.js
@@ -1,12 +1,40 @@
 var usagesModule = (function () {
+    const metrics_aws = ["s3", "ec2", "rds", "cost"]
+    const metrics_azure = ["vm", "storage", "account", "cost"]
+    const regions = ["east", "north", "ouest", "south"]
+    const departments = ["GTS", "BSC", "ITIM"]
+    const environments = ["PRD", "DEV", "HML"]
 
+    let metriclist = document.getElementById("metrics");
+    let regionlist = document.getElementById("regions");
+    let departmentlist = document.getElementById("departments");
+    let environmentlist = document.getElementById("environments");
 
     let init = function () {
+        populateSelect(metrics_aws, metriclist)
+        populateSelect(regions, regionlist)
+        populateSelect(departments, departmentlist)
+        populateSelect(environments, environmentlist)
+    }
 
+    let populateSelect = function (options, elementList) {
+        for (let option in options) {
+            var optionEl = document.createElement("option");
+            optionEl.text = options[option];
+            elementList.add(optionEl);
+        }
+    }
+
+    let refreshSelect = function (csp) {
+        metriclist.innerHTML = ""
+        if (csp === "aws") {
+            populateSelect(metrics_aws, metriclist)
+        } else {
+            populateSelect(metrics_azure, metriclist)
+        }
     }
 
     let getUsages = function () {
-        // var queryToSelect = document.getElementById("query-list").value;
 
 
         getGitHubRepo(variables)
@@ -14,8 +42,7 @@ var usagesModule = (function () {
                 res.json()
             )
             .then(res => {
-                // content = document.getElementById("content-json");
-                // content.innerHTML = JSON.stringify(res.data, undefined, 2);
+                draw_table()
             }
             );
     }
@@ -30,8 +57,48 @@ var usagesModule = (function () {
         )
     }
 
+    function drawTable() {
+        var array = [["S3", "N/A", "144", "MegaBytes"], ["Cost", "N/A", "144", "USD"], ["Ec2", "40", "N/A", "N/A"]]
+        var arrayHead = ["type", "count", "consumed", "unit"]
+        let form3 = document.getElementById('form-3')
+        let caption = document.createElement("caption")
+        caption.innerHTML = "last update : 2018-10-29"
+        let trhead = document.createElement("tr")
+
+        let thead = document.createElement("thead")
+        thead.appendChild(trhead)
+        let tbody = document.createElement("tbody")
+
+        let table = document.createElement('table')
+        table.classList.add("table", "table-striped")
+        table.appendChild(caption)
+        table.appendChild(thead)
+        table.appendChild(tbody)
+
+        form3.innerHTML = ''
+        form3.appendChild(table)
+
+        for (var i = 0; i < arrayHead.length; i++) {
+            var th = document.createElement('th')
+            th.innerHTML = arrayHead[i]
+            trhead.appendChild(th)
+        }
+        for (var i = 0; i < array.length; i++) {
+            let tr = document.createElement("tr")
+            tbody.appendChild(tr)
+
+            for (var j = 0; j < array[i].length; j++) {
+                var tdElement = document.createElement('td')
+                tdElement.innerHTML = array[i][j]
+                tr.appendChild(tdElement)
+            }
+        }
+    }
+
     return {
         getUsages: getUsages,
-        init: init
+        init: init,
+        drawTable: drawTable,
+        refreshSelect: refreshSelect
     };
 })();
--- Script/interaction.js
+++ Script/interaction.js
@@ -1,13 +1,27 @@
 usagesModule.init("")
 
-// document.getElementById('button-values')
-//     .addEventListener('click', function () {
-//         var result = usagesModule.getUsages();
-//     });
-let form2 = document.getElementById('form-2')
+
+let form1 = document.getElementById('form-1')
+let form3 = document.getElementById('form-3')
+
 let cspBtn = document.querySelectorAll('#form-1 button')
 for (var i = 0; i < cspBtn.length; i++) {
-    cspBtn[i].addEventListener('click', function(event) {
-            form2.setAttribute("data-csp",this.id);
+    cspBtn[i].addEventListener('click', function (event) {
+        form3.innerHTML = ""
+        reinit_checked()
+        usagesModule.refreshSelect(this.id)
+        form1.setAttribute("data-csp", this.id)
+        this.classList.add("checked")
     });
+}
+
+function reinit_checked() {
+    for (var i = 0; i < cspBtn.length; i++) {
+        cspBtn[i].classList.remove("checked")
+    }
+}
+
+let inputFields = document.querySelectorAll('input,select')
+for (var i = 0; i < inputFields.length; i++) {
+    inputFields[i].onchange = usagesModule.drawTable
 }
